07f3433353daf562d4b56914c1a1c037933b8d9b
Karpukhin-EV <evkarpukhin@mail.ru>
2022-03-23 23:00:38 +0300

    Inition Commit

---
+++ src/main/java/pro/sky/java/cource2/skyprospringhw4_1/SkyproSpringHw41Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.cource2.skyprospringhw4_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHw41Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproSpringHw41Application.class, args);
+    }
+
+}
